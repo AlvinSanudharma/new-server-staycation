79f269d2ec14c47240cba6a55964194b1f626243
unknown <alvinsanudharma@gmail.com>
2021-08-15 20:47:56 +0800

    CRUD Bank

--- models/Bank.js
+++ models/Bank.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const bankSchema = new Schema({
@@ -11,6 +11,10 @@ const bankSchema = new Schema({
     required: true,
   },
   name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
     type: String,
     required: true,
   },
