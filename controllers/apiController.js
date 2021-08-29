bdd9235562adee9cbb5d121352ac92a1a9fc8ca6
unknown <alvinsanudharma@gmail.com>
2021-08-29 15:58:31 +0800

    API Landing Page Project Staycation Part 3

---
+++ controllers/apiController.js
@@ -0,0 +1,44 @@
+const Item = require("../models/Item");
+const Treasure = require("../models/Activity");
+const Treveler = require("../models/Booking");
+const Category = require("../models/Category");
+
+module.exports = {
+  landingPage: async (req, res) => {
+    try {
+      const mostPicked = await Item.find()
+        .select("_id title country city price unit imageId")
+        .limit(5)
+        .populate({ path: "imageId", select: "_id imageUrl" });
+
+      const category = await Category.find()
+        .select("_id name")
+        .limit(3)
+        .populate({
+          path: "itemId",
+          select: "_id title country city isPopular imageId",
+          perDocumentLimit: 4,
+          populate: {
+            path: "imageId",
+            select: "_id imageUrl",
+            perDocumentLimit: 1,
+          },
+        });
+
+      const treveler = await Treveler.find();
+      const treasure = await Treasure.find();
+      const city = await Item.find();
+
+      //   console.log(mostPicked);
+      res.status(200).json({
+        hero: {
+          travelers: treveler.length,
+          treasure: treasure.length,
+          cities: city.length,
+        },
+        mostPicked,
+        category,
+      });
+    } catch (error) {}
+  },
+};
